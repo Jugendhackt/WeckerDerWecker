ee1cea5db492a32225403d9617be7aa0261bd7d4
NotARealPro <you@example.com>
2017-11-26 10:39:24 +0100

    Mit Servo

--- Arduino_Clock/Arduino_Clock.ino
+++ Arduino_Clock/Arduino_Clock.ino
@@ -2,12 +2,21 @@
 int Sekunden = 0;
 int Minuten = 0;
 int Stunden = 0;
+const int buttonPin = 8;
+int buttonState = 0;
+#include <Servo.h>
+int pos = 0;
+Servo myservo;
 void setup() {
+  myservo.attach(6);
+  pinMode(buttonPin, INPUT);
+  digitalWrite(buttonPin, HIGH);
   Serial.begin(9600);
 }
 
 void loop() {
-  delay(1000);
+  buttonState = digitalRead(buttonPin);
+  delay(100);
   Sekunden = Sekunden + 1;
   Serial.print(Stunden);
   Serial.print(" : ");
@@ -29,10 +38,24 @@ void loop() {
   //Wenn "if (Stunden > 6)" und "if (Minuten > 29) da steht, klingelt der Wecker um 7:30
   if (Stunden > -1) {
     if (Minuten > 0) {
-      noTone(11);
-      tone(6, 499, 140);
-      delay(100);
+      tone(13, 499, 140);
+    }
+
+    if (buttonState == LOW) {
+      noTone(13);
+      while (true) {};
+    }
+    if (Stunden > -1) {
+      if (Minuten > 1) {
+        for (pos = 0; pos <= 90; pos += 1) {
+          myservo.write(pos);
+          delay(15);
+          }
+        }
+      }
+
     }
   }
-}
+
+
 
